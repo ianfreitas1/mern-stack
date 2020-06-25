d7dad04582db7ce2bc0b6ae5d8e56076f901262c
Ian Freitas <ianfreitas1@poli.ufrj.br>
2020-06-25 19:36:11 -0300

    adding comments

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -5,7 +5,10 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from '../actions/types';
+import { post } from 'request';
 
 const initialState = {
   posts: [],
@@ -54,6 +57,23 @@ export default function (state = initialState, action) {
         posts: state.posts.map(post =>
           post._id === payload.postId ? { ...post, likes: payload.likes } : post
         ),
+        loading: false,
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            comment => comment._id !== payload
+          ),
+        },
         loading: false,
       };
     default:
